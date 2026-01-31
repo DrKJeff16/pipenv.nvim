31b14c51ec28ecb1bd85f8490c829a8e93e9fa0f
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-01-30 21:16:45 -0600

    feat(health): construct health file more extensively
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/pipenv/health.lua
+++ lua/pipenv/health.lua
@@ -1,15 +1,68 @@
+local Util = require('pipenv.util')
+
 ---@class Pipenv.Health
 local M = {}
 
-function M.check()
-  vim.health.start('pipenv')
+---@param exe string
+---@param idx integer
+---@return string version
+local function get_version(exe, idx)
+  Util.validate({
+    exe = { exe, { 'string' } },
+    idx = { idx, { 'number' } },
+  })
+  if not Util.is_int(idx) then
+    error(('Not an integer `%s`!'):format(idx), vim.log.levels.ERROR)
+  end
 
-  if vim.g.pipenv_setup == 1 then
-    vim.health.ok('`pipenv` has been setup!')
+  local out = vim.system({ exe, '--version' }):wait(1000).stdout
+  if not out or out == '' then
+    return ''
+  end
+  out = out:sub(1, out:len() - 1)
+
+  return ('%s'):format(vim.split(out, ' ', { trimempty = true })[idx])
+end
+
+function M.check()
+  vim.health.start('Setup')
+  if vim.g.pipenv_setup ~= 1 then
+    vim.health.error('`pipenv.nvim` has not been setup correctly!')
+    return
+  end
+  vim.health.ok('`pipenv.nvim` has been setup!')
+
+  vim.health.start('Requirements')
+  for _, exe in ipairs({ { 'python', 2 }, { 'pipenv', 3 } }) do
+    if not Util.executable(exe[1]) then
+      vim.health.error(('`%s` not found in `PATH`!'):format(exe[1]))
+      return
+    end
+    vim.health.ok(
+      ('`%s %s` found in `PATH`'):format(exe[1], get_version(vim.fn.exepath(exe[1]), exe[2]))
+    )
+    vim.health.info(('`%s`'):format(vim.fn.exepath(exe[1])))
+  end
+
+  vim.health.start('Environment')
+  vim.health.info("If warnings are raised in this section that doesn't mean the plugin won't work.")
+
+  local env = vim.fn.environ()
+  local ret = false
+  if not vim.fn.has_key(env, 'PIPENV_ACTIVE') or env.PIPENV_ACTIVE ~= '1' then
+    vim.health.warn('`$PIPENV_ACTIVE` not set!')
+    ret = true
+  end
+  if not vim.fn.has_key(env, 'VIRTUAL_ENV') or env.VIRTUAL_ENV ~= '1' then
+    vim.health.warn('`$VIRTUAL_ENV` not set!')
+    ret = true
+  end
+  if ret then
     return
   end
 
-  vim.health.error('`pipenv` has not been setup correctly!')
+  vim.health.ok('`$PIPENV_ACTIVE` is `1`')
+  vim.health.ok(('`$VIRTUAL_ENV` is `%s`'):format(env.VIRTUAL_ENV))
 end
 
 return M
