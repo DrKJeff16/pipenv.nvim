ce680020c2dd65409ab925f3e32d8618bc8f2f9f
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2026-02-06 13:31:38 -0600

    fix(picker): improve `picker.nvim` integration
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- lua/picker/sources/pipenv.lua
+++ lua/picker/sources/pipenv.lua
@@ -1,13 +1,20 @@
+---@module 'picker'
+
 local Util = require('pipenv.util')
+local ERROR = vim.log.levels.ERROR
 
 ---@param item 'run'|'uninstall'|'install'
 ---@param opts? Pipenv.RunOpts|Pipenv.UninstallOpts|Pipenv.InstallOpts
-local function gen_item(item, opts)
+local function run_item(item, opts)
   Util.validate({
     item = { item, { 'string' } },
     opts = { opts, { 'table', 'nil' }, true },
   })
   opts = opts or {}
+  if not vim.list_contains({ 'run', 'install', 'uninstall' }, item) then
+    vim.notify(('Bad item: `%s`'):format(item), ERROR)
+    return
+  end
 
   local Core = require('pipenv.core')
   if item == 'run' then
@@ -20,82 +27,47 @@ local function gen_item(item, opts)
     end)
     return
   end
-  if vim.list_contains({ 'install', 'uninstall' }, item) then
-    vim.ui.input(
-      { prompt = ('Type the packages to %s (separated by a space)'):format(item) },
-      function(input)
-        if item == 'install' then
-          ---@cast opts Pipenv.InstallOpts
-          Core.install(
-            (input and input ~= '') and vim.split(input, ' ', { plain = true, trimempty = true })
-              or nil,
-            opts
-          )
-          return
-        end
-
-        ---@cast opts Pipenv.UninstallOpts
-        Core.uninstall(
-          (input and input ~= '') and vim.split(input, ' ', { plain = true, trimempty = true })
-            or nil,
-          opts
-        )
+  ---@cast opts Pipenv.InstallOpts|Pipenv.UninstallOpts
+  vim.ui.input(
+    { prompt = ('Type the packages to %s (separated by a space)'):format(item) },
+    function(input)
+      if not input or input == '' then
+        return
       end
-    )
-    return
-  end
+      (item == 'install' and Core.install or Core.uninstall)(
+        vim.split(input, ' ', { plain = true, trimempty = true }),
+        opts
+      )
+    end
+  )
 end
 
 ---@return string[] actions
 local function get_all_actions()
   return {
     'clean',
-    'clean (verbose)',
     'edit',
     'graph',
     'install',
     'install (dev)',
     'install (pre)',
-    'install (verbose)',
     'install (dev,pre)',
-    'install (verbose,dev)',
-    'install (verbose,pre)',
-    'install (verbose,dev,pre)',
     'list installed',
     'list scripts',
     'lock',
-    'lock (dev)',
-    'lock (pre)',
-    'lock (verbose)',
-    'lock (dev,pre)',
-    'lock (verbose,dev)',
-    'lock (verbose,pre)',
-    'lock (verbose,dev,pre)',
     'requirements',
     'requirements (dev)',
-    'requirements (pre)',
-    'requirements (dev,pre)',
     'run',
-    'run (verbose)',
+    'scripts',
     'sync',
     'sync (dev)',
     'sync (pre)',
-    'sync (verbose)',
     'sync (dev,pre)',
-    'sync (verbose,dev)',
-    'sync (verbose,pre)',
-    'sync (verbose,dev,pre)',
-    'scripts',
     'uninstall',
     'uninstall (dev)',
     'uninstall (pre)',
-    'uninstall (verbose)',
     'uninstall (dev,pre)',
-    'uninstall (verbose,dev)',
-    'uninstall (verbose,pre)',
-    'uninstall (verbose,dev,pre)',
     'verify',
-    'verify (verbose)',
   }
 end
 
@@ -117,8 +89,45 @@ local function gen_items(source)
   return items
 end
 
+---@class Picker.Sources.PipenvOpts
+---@field dev? boolean
+---@field pre? boolean
+---@field verbose? boolean
+---@field python? string
+
 ---@class Picker.Sources.Pipenv
 local M = {}
+
+M.verbose = true ---@type boolean
+M.dev = false ---@type boolean
+M.pre = false ---@type boolean
+M.python = nil ---@type string|nil
+
+---@param opt Picker.Sources.PipenvOpts
+function M.set(opt)
+  if not opt or vim.tbl_isempty(opt) then
+    return
+  end
+
+  Util.validate({
+    dev = { opt.dev, { 'boolean', 'nil' }, true },
+    pre = { opt.pre, { 'boolean', 'nil' }, true },
+    python = { opt.python, { 'string', 'nil' }, true },
+  })
+
+  if opt.verbose ~= nil then
+    M.verbose = opt.verbose
+  end
+  if opt.dev ~= nil then
+    M.dev = opt.dev
+  end
+  if opt.pre ~= nil then
+    M.pre = opt.pre
+  end
+  if opt.python and opt.python ~= '' then
+    M.python = opt.python
+  end
+end
 
 function M.get()
   return gen_items(get_all_actions())
@@ -128,28 +137,29 @@ end
 function M.default_action(entry)
   local Core = require('pipenv.core')
   local value = vim.split(entry.value, ' ', { plain = true, trimempty = true })
-  if #value == 1 then
-    Core[value[1]]()
+  if vim.tbl_isempty(value) then
+    vim.notify('Empty action!', ERROR)
     return
   end
+
   if value[1] == 'list' and #value == 2 then
     Core[table.concat(value, '_')]()
     return
   end
   local action = value[1] ---@type 'clean'|'install'|'lock'|'uninstall'|'sync'|'verify'|'run'|'requirements'
-  local opts = {}
+  local opts = { verbose = M.verbose, dev = M.dev, pre = M.pre, python = M.python }
   if #value == 2 then
     value[2] = value[2]:sub(2, value[2]:len())
     value[2] = value[2]:sub(1, value[2]:len() - 1)
     for _, choice in ipairs(vim.split(value[2], ',', { plain = true, trimempty = true })) do
-      ---@cast choice 'dev'|'pre'|'verbose'
+      ---@cast choice 'dev'|'pre'
       opts[choice] = true
     end
   end
 
   if vim.list_contains({ 'run', 'install', 'uninstall' }, action) then
     ---@cast action 'run'|'install'|'uninstall'
-    gen_item(action, opts)
+    run_item(action, opts)
     return
   end
 
@@ -159,7 +169,7 @@ function M.default_action(entry)
     return
   end
 
-  vim.notify(('Bad entry `%s`!'):format(table.concat(value, ' ')), vim.log.levels.ERROR)
+  vim.notify(('Bad entry `%s`!'):format(table.concat(value, ' ')), ERROR)
 end
 
 return M
